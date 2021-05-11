0aff4b34e33315ffcc09ceeeacc9ba4daccc05b7
Alexander <alexander.bohl@smail.inf.h-brs.de>
2021-05-11 21:16:36 +0200

    ParkhausServiceIF hinzugefügt

--- src/main/kotlin/de/hbrs/team7/se1_starter_repo/ParkhausServiceIF.kt
+++ src/main/kotlin/de/hbrs/team7/se1_starter_repo/ParkhausServiceIF.kt
@@ -1,10 +1,28 @@
 package de.hbrs.team7.se1_starter_repo
 
+import java.util.ArrayList
+
 interface ParkhausServiceIF {
 
     var initNumber: Int;
 
     public fun iterInit() = initNumber++;
+
+    open fun totalCarCount(ID: String): Int;
+
+    open fun totalUsers(ID: String): Int;
+
+    open fun average(ID: String): Double?;
+
+    open fun leaveCar(ID: String, params: Array<String>);
+
+    open fun sum(ID: String): Double;
+
+    open fun addCar(ID: String, params: Array<String>);
+
+    open fun currentCars(NAME: String): ArrayList<CarIF>?;
+
+
 
 
 }
