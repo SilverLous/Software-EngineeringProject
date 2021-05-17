e2584ca6dbcb301537770b74b03100a383fc23db
Thomas Gerlach <t.g.g@web.de>
2021-05-17 19:45:18 +0200

    add(reference implementation for uml class)

---
+++ src/main/kotlin/de/hbrs/team7/se1_starter_repo/Ticket.kt
@@ -0,0 +1,13 @@
+package de.hbrs.team7.se1_starter_repo
+
+import java.util.*
+
+class Ticket {
+
+    lateinit var Ticketnummer: String
+    lateinit var Kennzeichen: String
+    lateinit var Ausstellungsdatum: Date
+    lateinit var Ausfahrdatum: Date
+    var Preisklasse: Int = 0
+
+}
