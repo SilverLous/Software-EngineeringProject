ca12184daf73fa879bd04b445a62ed0e5b2d7bd4
Alexander <alexander.bohl@smail.inf.h-brs.de>
2021-05-21 10:45:33 +0200

    #9 Datentyp von Kundennummer in Dauerticket auf int geändert, Klasse Kunde hinzugefügt

--- src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/DauerTicket.kt
+++ src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/DauerTicket.kt
@@ -2,13 +2,13 @@ package de.hbrs.team7.se1_starter_repo.entities
 
 import java.util.*
 
-class DauerTicket(private var ablaufDatum: Date, private var kundennummer: String) : Ticket() {
+class DauerTicket(private var ablaufDatum: Date, private var kundennummer: Int) : Ticket() {
 
     fun getAblaufdatum(): Date {
         return ablaufDatum
     }
 
-    fun getKundennummer():String {
+    fun getKundennummer():Int {
         return kundennummer
     }
 }
---
+++ src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/Kunde.kt
@@ -0,0 +1,6 @@
+package de.hbrs.team7.se1_starter_repo.entities
+
+class Kunde(kundennummer: Int, bezahlinformationen: String, kontaktinformationen: String) {
+    var auto_verlaengerung: Boolean = false
+
+}
