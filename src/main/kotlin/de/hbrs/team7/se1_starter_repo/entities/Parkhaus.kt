d9b3b95fafc9667105139670a0e6392f51f5807e
Alexander <alexander.bohl@smail.inf.h-brs.de>
2021-05-25 16:24:48 +0200

    #9 Implementation von Parkhaus, ParkhausEbene

--- src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/Parkhaus.kt
+++ src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/Parkhaus.kt
@@ -1,4 +1,22 @@
 package de.hbrs.team7.se1_starter_repo.entities
 
 class Parkhaus {
+
+    val ebenen = mutableListOf<ParkhausEbene>()
+
+    fun addParkhausEbene(ebene: ParkhausEbene) {
+        ebenen.add(ebene)
+    }
+
+    fun removeParkhausEbene(ebene: ParkhausEbene): Boolean {
+        return ebenen.remove(ebene)
+    }
+
+    fun autoEinfahren(ebene: Int): Int {
+        return ebenen[ebene].autoEinfahren()
+    }
+
+    fun autoAusfahren(ebene: Int): Int {
+        return ebenen[ebene].autoAusfahren()
+    }
 }
--- src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/ParkhausEbene.kt
+++ src/main/kotlin/de/hbrs/team7/se1_starter_repo/entities/ParkhausEbene.kt
@@ -1,4 +1,48 @@
 package de.hbrs.team7.se1_starter_repo.entities
 
-class ParkhausEbene {
+class ParkhausEbene(private val nummer: Int) {
+
+    private var gesamtPlaetze: Int = 0
+    private var freiePlaetze: Int = 0
+
+    fun getGesamtPlaetze(): Int {
+        return gesamtPlaetze
+    }
+
+    fun setGesamtPlaetze(neuePlaetze: Int): Int {
+        val differenz = gesamtPlaetze - neuePlaetze
+        if (differenz > freiePlaetze) {
+            return -1
+        } else {
+            gesamtPlaetze = neuePlaetze
+            return 0
+        }
+    }
+
+    fun getFreiePlaetze(): Int {
+        return freiePlaetze
+    }
+
+    fun getBelegtePlaetze(): Int {
+        return gesamtPlaetze - freiePlaetze
+    }
+
+    fun autoEinfahren(): Int {
+        if (freiePlaetze == 0) {
+            return -1
+        } else {
+            freiePlaetze--
+            return freiePlaetze
+        }
+    }
+
+    fun autoAusfahren(): Int {
+        if (getBelegtePlaetze() >= gesamtPlaetze) {
+            return -1
+        } else {
+            freiePlaetze++
+            return freiePlaetze
+        }
+    }
+
 }
