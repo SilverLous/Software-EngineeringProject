7a159c347ee09d020892c90d4beee790b0aa705e
Thomas Gerlach <t.g.g@web.de>
2021-05-27 23:24:52 +0200

    add(basic tests)

--- src/test/kotlin/de/hbrs/team7/se1_starter_repo/ParkhausDatabaseGlobalJavaTest.java
+++ src/test/kotlin/de/hbrs/team7/se1_starter_repo/ParkhausDatabaseGlobalJavaTest.java
@@ -1,5 +1,6 @@
 package de.hbrs.team7.se1_starter_repo;
 
+import de.hbrs.team7.se1_starter_repo.entities.Parkhaus;
 import de.hbrs.team7.se1_starter_repo.services.DatabaseServiceGlobal;
 import de.hbrs.team7.se1_starter_repo.services.ParkhausServiceGlobal;
 import de.hbrs.team7.se1_starter_repo.services.ParkhausServiceSession;
@@ -9,8 +10,7 @@ import jakarta.inject.Inject;
 import org.jboss.weld.junit5.auto.ActivateScopes;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 
 // https://github.com/weld/weld-junit/blob/master/junit5/README.md
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.Test;
 @ActivateScopes({ SessionScoped.class, ApplicationScoped.class })
 @AddBeanClasses({ ParkhausServiceSession.class, ParkhausServiceGlobal.class, DatabaseServiceGlobal.class})
 public class ParkhausDatabaseGlobalJavaTest {
-
 
     @Inject
     ParkhausServiceSession parkhausServiceSession;
@@ -39,6 +38,57 @@ public class ParkhausDatabaseGlobalJavaTest {
     public void sessionInitTest() {
         assert databaseServiceGlobal != null;
     }
+
+    @Nested
+    @DisplayName("Simple test chain")
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class basicFunctionalityTest {
+
+        private Parkhaus p;
+
+
+        @Test
+        @Order(1)
+        @DisplayName("Testen der Insert Funktion")
+        public void insertTest(){
+            // Problem: auf ID kann nicht direkt zugegriffen werden
+            Parkhaus p_test = new Parkhaus("TestStadt" );
+            this.p =  databaseServiceGlobal.persistEntity(p_test);
+
+            Assertions.assertNotNull(p.getId());
+            System.out.println(p.getId());
+            // assertAll
+        }
+
+        @Test
+        @Order(2)
+        @DisplayName("Testen der find Funktion")
+        public void findTest(){
+
+            long p_id = p.getId();
+            Parkhaus p_test = databaseServiceGlobal.findByID(p_id, Parkhaus.class);
+
+            Assertions.assertEquals(p, p_test);
+        }
+
+        @Test
+        @Order(3)
+        @DisplayName("Testen der delete Funktion")
+        public void deleteTest(){
+
+            long p_id = p.getId();
+            databaseServiceGlobal.deleteByID(p_id, Parkhaus.class);
+            Parkhaus p_test = databaseServiceGlobal.findByID(p_id, Parkhaus.class);
+
+            Assertions.assertNull(p_test);
+        }
+
+
+    }
+
+
+
+
 
 
 }
