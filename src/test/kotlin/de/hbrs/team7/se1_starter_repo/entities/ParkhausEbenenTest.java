cd94801501e63e0eed6be7bdffd6e487b913b7fa
Alexander <alexander.bohl@smail.inf.h-brs.de>
2021-07-08 15:05:29 +0200

    SonarQube-Fehler gefixt

--- src/test/kotlin/de/hbrs/team7/se1_starter_repo/entities/ParkhausEbenenTest.java
+++ src/test/kotlin/de/hbrs/team7/se1_starter_repo/entities/ParkhausEbenenTest.java
@@ -16,12 +16,12 @@ import java.util.HashMap;
 public class ParkhausEbenenTest {
 
     static final String EBENEN_NAMEN = "TestBundesland";
-    static final int MAX_PLÄTZE = 12;
-    static final int ÖFFNUNGSZEIT = 6;
+    static final int MAX_PLAETZE = 12;
+    static final int OEFFNUNGSZEIT = 6;
     static final int LADENSCHLUSS = 24;
-    static final int VERZÖGERUNG = 200;
+    static final int VERZOEGERUNG = 200;
     static final int SIMULATIONS_GESCHWINDIGKEIT = 1;
-    static final HashMap<String, Double> FAHRZEUG_PREISE = new HashMap<String, Double>();
+    static final HashMap<String, Double> FAHRZEUG_PREISE = new HashMap<>();
 
     @Test
     @DisplayName("Teste Statischen Fabrikator")
@@ -31,17 +31,17 @@ public class ParkhausEbenenTest {
         FAHRZEUG_PREISE.put("Pickup", 1.2);
         FAHRZEUG_PREISE.put("SUV", 1.5);
 
-        ParkhausEbeneConfigDTO testConfig = new ParkhausEbeneConfigDTO(EBENEN_NAMEN, MAX_PLÄTZE, ÖFFNUNGSZEIT,
-                LADENSCHLUSS, VERZÖGERUNG, SIMULATIONS_GESCHWINDIGKEIT, FAHRZEUG_PREISE, null);
+        ParkhausEbeneConfigDTO testConfig = new ParkhausEbeneConfigDTO(EBENEN_NAMEN, MAX_PLAETZE, OEFFNUNGSZEIT,
+                LADENSCHLUSS, VERZOEGERUNG, SIMULATIONS_GESCHWINDIGKEIT, FAHRZEUG_PREISE, null);
 
 
         ParkhausEbene pe = ParkhausEbene.Companion.ausEbenenConfig(testConfig);
 
         Assertions.assertEquals(EBENEN_NAMEN, pe.getName());
-        Assertions.assertEquals(MAX_PLÄTZE, pe.getMaxPlätze());
-        Assertions.assertEquals(ÖFFNUNGSZEIT, pe.getöffnungszeit());
+        Assertions.assertEquals(MAX_PLAETZE, pe.getMaxPlätze());
+        Assertions.assertEquals(OEFFNUNGSZEIT, pe.getöffnungszeit());
         Assertions.assertEquals(LADENSCHLUSS, pe.getLadenschluss());
-        Assertions.assertEquals(VERZÖGERUNG, pe.getVerzögerung());
+        Assertions.assertEquals(VERZOEGERUNG, pe.getVerzögerung());
         Assertions.assertEquals(SIMULATIONS_GESCHWINDIGKEIT, pe.getSimulationsGeschwindigkeit());
         Assertions.assertNull(pe.getParkhaus());
 
