d6fcb912aefc392167027a97f14a64e7c6ea2baf
Thomas Gerlach <t.g.g@web.de>
2021-07-08 17:15:17 +0200

    fixed(sonarQube error)

--- src/test/kotlin/de/hbrs/team7/se1_starter_repo/entities/ParkhausEbenenTest.java
+++ src/test/kotlin/de/hbrs/team7/se1_starter_repo/entities/ParkhausEbenenTest.java
@@ -16,10 +16,10 @@ import java.util.HashMap;
 public class ParkhausEbenenTest {
 
     static final String EBENENNAMEN = "TestBundesland";
-    static final int MAXPLÄTZE = 12;
-    static final int ÖFFNUNGSZEIT = 6;
+    static final int MAXPLAETZE = 12;
+    static final int OEFFNUNGSZEIT = 6;
     static final int LADENSCHLUSS = 24;
-    static final int VERZÖGERUNG = 200;
+    static final int VERZOEGERUNG = 200;
     static final int SIMULATIONSGESCHWINDIGKEIT = 1;
     static final int ZEITVERSCHUB = 0;
     static final HashMap<String, Double> FAHRZEUGPREISE = new HashMap<>();
@@ -33,17 +33,17 @@ public class ParkhausEbenenTest {
         FAHRZEUGPREISE.put("SUV", 1.5);
 
         ParkhausEbeneConfigDTO testConfig = new ParkhausEbeneConfigDTO(
-                EBENENNAMEN, MAXPLÄTZE, ÖFFNUNGSZEIT,
-                LADENSCHLUSS, VERZÖGERUNG, ZEITVERSCHUB, SIMULATIONSGESCHWINDIGKEIT, FAHRZEUGPREISE, null);
+                EBENENNAMEN, MAXPLAETZE, OEFFNUNGSZEIT,
+                LADENSCHLUSS, VERZOEGERUNG, ZEITVERSCHUB, SIMULATIONSGESCHWINDIGKEIT, FAHRZEUGPREISE, null);
 
 
         ParkhausEbene pe = ParkhausEbene.Companion.ausEbenenConfig(testConfig);
 
         Assertions.assertEquals(EBENENNAMEN, pe.getName());
-        Assertions.assertEquals(MAXPLÄTZE, pe.getMaxPlätze());
-        Assertions.assertEquals(ÖFFNUNGSZEIT, pe.getöffnungszeit());
+        Assertions.assertEquals(MAXPLAETZE, pe.getMaxPlätze());
+        Assertions.assertEquals(OEFFNUNGSZEIT, pe.getöffnungszeit());
         Assertions.assertEquals(LADENSCHLUSS, pe.getLadenschluss());
-        Assertions.assertEquals(VERZÖGERUNG, pe.getVerzögerung());
+        Assertions.assertEquals(VERZOEGERUNG, pe.getVerzögerung());
         Assertions.assertEquals(SIMULATIONSGESCHWINDIGKEIT, pe.getSimulationsGeschwindigkeit());
         Assertions.assertNull(pe.getParkhaus());
 
