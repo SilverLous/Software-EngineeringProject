d05bd5dbbbba26e793b5b0b119db3e2ef82b2d84
Thomas Gerlach <t.g.g@web.de>
2021-07-08 22:08:20 +0200

    add(better git statistic caclulation)

--- cities_python_analysis/git_analysis.py
+++ cities_python_analysis/git_analysis.py
@@ -5,23 +5,27 @@ import pandas as pd
 
 # git-fame --excl='cities_python_analysis/*|dokumentation/*|src/main/kotlin/de/hbrs/team7/se1_starter_repo/services/ParkhausServiceGlobal.kt|src/main/kotlin/de/hbrs/team7/se1_starter_repo/ParkhausServiceSession.kt'
 
+# better one
+
+# git-fame --since=2021-04-30 --incl ".kt|.java|.jsp" --excl='src/main/kotlin/de/hbrs/team7/se1_starter_repo/services/ParkhausServiceGlobal.kt'
+
 """
-Total commits: 251
-Total ctimes: 1457
-Total files: 96
-Total loc: 7132
+Total commits: 319
+Total ctimes: 1699
+Total files: 85
+Total loc: 4908
 | Author         |   loc |   coms |   fils |  distribution   |
 |:---------------|------:|-------:|-------:|:----------------|
-| Thomas Gerlach |  5250 |     99 |     45 | 73.6/39.4/46.9  |
-| Lukas Gerlach  |  1139 |     79 |     17 | 16.0/31.5/17.7  |
-| Alexander      |   679 |     65 |     26 | 9.5/25.9/27.1   |
-| Eileen Hanz    |    64 |      8 |      8 | 0.9/ 3.2/ 8.3   |
+| Thomas Gerlach |  2708 |    120 |     34 | 55.2/37.6/40.0  |
+| Lukas Gerlach  |  1387 |     97 |     22 | 28.3/30.4/25.9  |
+| Alexander      |   760 |     94 |     24 | 15.5/29.5/28.2  |
+| Eileen Hanz    |    53 |      8 |      5 | 1.1/ 2.5/ 5.9   |
 """
 
 git_data = {
   "Author": ["Thomas Gerlach", "Lukas Gerlach", "Alexander", "Eileen Hanz"],
-  "loc": [5225, 1181, 594, 64],
-  "coms": [99, 79, 65, 8],
+  "loc": [2708, 1387, 760, 53],
+  "coms": [120, 97, 94, 8],
   "fils": [45, 17, 26, 8]
 }
 
@@ -32,9 +36,9 @@ df.set_index(["Author"])
 # https://vm-2d21.inf.h-brs.de/mk_se1_ss21_Team_7/mk_se1_ss21_Team_7/-/commit/ac034d47020d3addde002be68ee34d3ea615e363
 # https://vm-2d21.inf.h-brs.de/mk_se1_ss21_Team_7/mk_se1_ss21_Team_7/-/commit/8778058029002c89c727c6d1f0d9aa70cc1715e1
 
-städte_lines = 1610
+#städte_lines = 1610
 
-df.loc[0, "loc"] = df.loc[0, "loc"] - (städte_lines * 2)
+#df.loc[0, "loc"] = df.loc[0, "loc"] - (städte_lines * 1)
 
 
 # https://vm-2d21.inf.h-brs.de/mk_se1_ss21_Team_7/mk_se1_ss21_Team_7/-/commit/321069e9050213f11e8c5aca1edf05bf147ed605
@@ -42,5 +46,13 @@ df.loc[0, "loc"] = df.loc[0, "loc"] - (städte_lines * 2)
 svg_code = 887
 
 df.loc[0, "loc"] = df.loc[0, "loc"] - svg_code
+
+# dotsvg_code = 2158
+
+# df.loc[0, "loc"] = df.loc[0, "loc"] - dotsvg_code
+
+df["codedist"] = df["loc"] / df["loc"].sum() * 100
+
+df["commitdist"] = df["coms"] / df["coms"].sum() * 100
 
 print(df)
