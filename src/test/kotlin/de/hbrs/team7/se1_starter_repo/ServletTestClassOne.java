6444e305ad3e430ccd6310db181360b709c8546e
Alexander <alexander.bohl@smail.inf.h-brs.de>
2021-07-09 09:18:37 +0200

    arquillian-Grundgerüst beginnen

---
+++ src/test/kotlin/de/hbrs/team7/se1_starter_repo/ServletTestClassOne.java
@@ -0,0 +1,35 @@
+package de.hbrs.team7.se1_starter_repo;
+import java.net.URL;
+
+import de.hbrs.team7.se1_starter_repo.servlets.Level1Servlet;
+import de.hbrs.team7.se1_starter_repo.servlets.ParkhausServlet;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+public class ServletTestClassOne {
+    @Deployment(testable = false)
+    public static WebArchive getTestArchive() {
+        return ShrinkWrap.create(WebArchive.class, "team7Parkhaus.war").addClass(ParkhausServlet.class);
+    }
+
+    @ArquillianResource
+    URL deploymentUrl;
+
+    @Test
+    public void shouldBeAbleToInvokeServletInDeployedWebApp() throws Exception {
+        deploymentUrl = new URL( "http","localhost",8084,"team7Parkhaus");
+        String requestUrl = deploymentUrl + "level1-servlet?" + "cmd" + "=einnahmenueberbundesland";
+        String body = (new URL(requestUrl).getContent()).toString();
+        System.out.println(body);
+        //Assert.assertEquals("Verify that the servlet was deployed and returns expected result", "hello", body);
+    }
+}
