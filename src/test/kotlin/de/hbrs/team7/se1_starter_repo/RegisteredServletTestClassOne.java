ad4ffcdf5b62f31ada0339a25c5c584acf26eb1d
Thomas Gerlach <t.g.g@web.de>
2021-07-11 14:23:10 +0200

    add(confi tests)

--- src/test/kotlin/de/hbrs/team7/se1_starter_repo/RegisteredServletTestClassOne.java
+++ src/test/kotlin/de/hbrs/team7/se1_starter_repo/RegisteredServletTestClassOne.java
@@ -1,23 +1,33 @@
 package de.hbrs.team7.se1_starter_repo;
-import java.net.URL;
 
 import de.hbrs.team7.se1_starter_repo.servlets.Level1Servlet;
-import de.hbrs.team7.se1_starter_repo.servlets.ParkhausServlet;
-import org.jboss.arquillian.junit5.ArquillianExtension;
-import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.protocol.servlet5.arq514hack.descriptors.api.web.WebAppDescriptor;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.net.URL;
+
 @ExtendWith(ArquillianExtension.class)
-@RunAsClient
-public class ServletTestClassOne {
+//@RunAsClient
+public class RegisteredServletTestClassOne {
     @Deployment(testable = false)
     public static WebArchive getTestArchive() {
-        return ShrinkWrap.create(WebArchive.class, "team7Parkhaus.war").addClass(Level1Servlet.class);
+        return ShrinkWrap.create(WebArchive.class, "team7Parkhaus.war")
+                .addClass(Level1Servlet.class)
+                .setWebXML(new StringAsset(Descriptors.create(WebAppDescriptor.class)
+                        .servlet(Level1Servlet.class.getSimpleName())
+                        .servletClass(Level1Servlet.class.getName())
+                        .mapping()
+                        .urlPattern("/level1-servlet").exportAsString()
+                        ));
     }
 
     @ArquillianResource(Level1Servlet.class)
