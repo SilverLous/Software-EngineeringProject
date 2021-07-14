0f04833c2c781ecc20eac344c1879716a95a859a
Lukas Gerlach <lukas.gerlach@smail.inf.h-brs.de>
2021-07-14 12:38:31 +0200

    -added some more graphs

--- cities_python_analysis/git_analysis_graphs.py
+++ cities_python_analysis/git_analysis_graphs.py
@@ -3,9 +3,22 @@ import matplotlib.pyplot as plt
 
 import git_analysis as git_data
 
-data = git_data.df_surv.drop(3)
-for col in data.columns:
+data_surv = git_data.df_surv.drop(3)
+data_ins = git_data.df_ins.drop(3)
+length = len(data_surv.columns)
+for col in data_surv.columns:
     if col != "Author":
-        plt.pie(data[col], labels=data["Author"], autopct="%.2f")
+        plt.pie(data_surv[col], labels=data_surv["Author"], autopct="%.2f")
         plt.title(col)
         plt.show()
+plt.bar(data_surv["Author"], data_surv["loc"]/data_surv["coms"])
+plt.title("Surviving lines per commit")
+plt.xlabel("Author")
+plt.ylabel("Commits")
+plt.show()
+
+plt.bar(data_surv["Author"], data_surv["loc"]/data_ins["loc"])
+plt.title("Prozent überlebender Lines")
+plt.xlabel("Author")
+plt.ylabel("Commits")
+plt.show()
