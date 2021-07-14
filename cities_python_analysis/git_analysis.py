c6ca45988928d2b22d7f8b01507f75df02c68d0b
Thomas Gerlach <t.g.g@web.de>
2021-07-14 12:06:12 +0200

    add(experimental data)

--- cities_python_analysis/git_analysis.py
+++ cities_python_analysis/git_analysis.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 # better one
 
+
 # git-fame --since=2021-04-30 --incl ".kt|.java|.jsp" --excl='src/main/kotlin/de/hbrs/team7/se1_starter_repo/services/ParkhausServiceGlobal.kt'
 
 """
@@ -22,37 +23,79 @@ Total loc: 4908
 | Eileen Hanz    |    53 |      8 |      5 | 1.1/ 2.5/ 5.9   |
 """
 
-git_data = {
+git_surviving_data = {
   "Author": ["Thomas Gerlach", "Lukas Gerlach", "Alexander", "Eileen Hanz"],
-  "loc": [2708, 1387, 760, 53],
-  "coms": [120, 97, 94, 8],
-  "fils": [45, 17, 26, 8]
+  "loc": [2800, 1368, 935, 13],
+  "coms": [123, 98, 114, 8],
+  "fils": [34, 20, 24, 3]
 }
 
-df = pd.DataFrame(git_data)
-df.set_index(["Author"])
 
+# git-fame --loc=ins --excl='src/main/kotlin/de/hbrs/team7/se1_starter_repo/services/ParkhausServiceGlobal.kt|cities_python_analysis|.*'
+
+"""
+Total commits: 346
+Total ctimes: 895
+Total files: 261
+Total loc: 45366
+| Author         |   loc |   coms |   fils |  distribution   |
+|:---------------|------:|-------:|-------:|:----------------|
+| Thomas Gerlach | 38784 |    126 |    115 | 85.5/36.4/44.1  |
+| Lukas Gerlach  |  3831 |     98 |     64 | 8.4/28.3/24.5   |
+| Alexander      |  2515 |    114 |     65 | 5.5/32.9/24.9   |
+| Eileen Hanz    |   236 |      8 |     17 | 0.5/ 2.3/ 6.5   |
+"""
+
+git_ins_data = {
+  "Author": ["Thomas Gerlach", "Lukas Gerlach", "Alexander", "Eileen Hanz"],
+  "loc": [38784, 3831, 2515, 236],
+  "coms": [126, 98, 114, 8],
+  "fils": [115, 64, 65, 17]
+}
+
+df_surv = pd.DataFrame(git_surviving_data)
+df_surv.set_index(["Author"])
+
+df_ins = pd.DataFrame(git_ins_data)
+df_ins.set_index(["Author"])
 
 # https://vm-2d21.inf.h-brs.de/mk_se1_ss21_Team_7/mk_se1_ss21_Team_7/-/commit/ac034d47020d3addde002be68ee34d3ea615e363
 # https://vm-2d21.inf.h-brs.de/mk_se1_ss21_Team_7/mk_se1_ss21_Team_7/-/commit/8778058029002c89c727c6d1f0d9aa70cc1715e1
 
-#städte_lines = 1610
+städte_lines = 1610
 
-#df.loc[0, "loc"] = df.loc[0, "loc"] - (städte_lines * 1)
+df_ins.loc[0, "loc"] = df_ins.loc[0, "loc"] - (städte_lines * 2)
 
 
 # https://vm-2d21.inf.h-brs.de/mk_se1_ss21_Team_7/mk_se1_ss21_Team_7/-/commit/321069e9050213f11e8c5aca1edf05bf147ed605
 
 svg_code = 887
 
-df.loc[0, "loc"] = df.loc[0, "loc"] - svg_code
+df_surv.loc[0, "loc"] = df_surv.loc[0, "loc"] - svg_code
 
-# dotsvg_code = 2158
+# copy is two insertions and one deletion
+df_ins.loc[0, "loc"] = df_ins.loc[0, "loc"] - (svg_code * 2)
 
-# df.loc[0, "loc"] = df.loc[0, "loc"] - dotsvg_code
 
-df["codedist"] = df["loc"] / df["loc"].sum() * 100
 
-df["commitdist"] = df["coms"] / df["coms"].sum() * 100
+dotsvg_code = 2158
 
-print(df)
+df_ins.loc[0, "loc"] = df_ins.loc[0, "loc"] - dotsvg_code
+
+
+worldcities_csv = 26570
+
+df_ins.loc[0, "loc"] = df_ins.loc[0, "loc"] - worldcities_csv
+
+
+df_surv["codedist"] = df_surv["loc"] / df_surv["loc"].sum() * 100
+
+df_surv["commitdist"] = df_surv["coms"] / df_surv["coms"].sum() * 100
+
+
+df_ins["codedist"] = df_ins["loc"] / df_ins["loc"].sum() * 100
+
+df_ins["commitdist"] = df_ins["coms"] / df_ins["coms"].sum() * 100
+
+print(df_surv)
+print(df_ins)
--- cities_python_analysis/git_analysis_graphs.py
+++ cities_python_analysis/git_analysis_graphs.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import git_analysis as git_data
 
-data = git_data.df.drop(3)
+data = git_data.df_surv.drop(3)
 for col in data.columns:
     if col != "Author":
         plt.pie(data[col], labels=data["Author"], autopct="%.2f")
